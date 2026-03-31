f11753b6926b6cf185c892ff0c62e48be71c9b96
MrKay12 <quentin.lepoutre12@gmail.com>
2026-03-31 14:14:15 +0200

    refactor: make shared directory setup script dynamic with parameters

--- linux_security/1x00_linux_fundamentals/6-setup_shared.sh
+++ linux_security/1x00_linux_fundamentals/6-setup_shared.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p "/shared/devs" && chgrp "developers" "/shared/devs" && chmod 2775 "/shared/devs" && chmod +t "/shared/devs"
+mkdir -p "$1" && chgrp "$2" "$1" && chmod 2775 "$1" && chmod +t "$1"
