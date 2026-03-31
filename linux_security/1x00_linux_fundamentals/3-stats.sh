eafb55f4f05f8405474e09f98baca8845e031f8c
MrKay12 <quentin.lepoutre12@gmail.com>
2026-03-31 11:08:13 +0200

    change ls command from -al to -l

--- linux_security/1x00_linux_fundamentals/3-stats.sh
+++ linux_security/1x00_linux_fundamentals/3-stats.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls -al "$1" | awk '{print $3}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
+ls -l "$1" | awk '{print $3}' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}'
