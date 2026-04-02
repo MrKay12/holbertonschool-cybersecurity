9deb6fbd0680b2f3e9b7555fd7dd55e0247ea70e
MrKay12 <quentin.lepoutre12@gmail.com>
2026-04-02 16:41:23 +0200

    refactor: simplify audit UID script by removing usage check

--- linux_security/1x02_identity_management/0-audit_uid.sh
+++ linux_security/1x02_identity_management/0-audit_uid.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
-  echo "Usage: $0 /etc/passwd"
-  exit 1
-fi
-
-PASSWD_FILE="$1"
-
-awk -F: '($3 == 0) && ($1 != "root") { print $1 }' "$PASSWD_FILE"
+awk -F: '($3==0)&&($1!="root"){print $1}' "$1"
