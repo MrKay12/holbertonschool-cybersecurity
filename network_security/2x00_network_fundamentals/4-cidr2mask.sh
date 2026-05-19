ada8d2e35da763ff95bd9ce7bce05dda57fb4512
MrKay12 <quentin.lepoutre12@gmail.com>
2026-05-19 13:27:04 +0000

    fix: improve logic for CIDR to subnet mask conversion in 4-cidr2mask.sh

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-n=$1; for i in 1 2 3 4; do [ $n -ge 8 ] && o=255 || o=$((256-2**(8-n))); printf "%d" $o; [ $i -lt 4 ] && printf "."; n=$((n-8)); done
+n=$1; for i in 1 2 3 4; do [ $n -ge 8 ] && o=255 || { [ $n -le 0 ] && o=0 || o=$((256-2**(8-n))); }; printf "%d" $o; [ $i -lt 4 ] && printf "."; n=$((n-8)); done
