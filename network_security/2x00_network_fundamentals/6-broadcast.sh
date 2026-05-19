98bf69a34ea307fa6211116a9d3b5c7f224e2095
MrKay12 <quentin.lepoutre12@gmail.com>
2026-05-19 13:39:26 +0000

    fix: update IFS usage in 6-broadcast.sh for improved readability

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS=. read a b c d e f g h <<< "${1//./ } ${2//./ }"; printf "%d.%d.%d.%d" $((a|255-e)) $((b|255-f)) $((c|255-g)) $((d|255-h))
+read -r a b c d e f g h <<< "${1//./ } ${2//./ }"; printf "%d.%d.%d.%d" $((a|255-e)) $((b|255-f)) $((c|255-g)) $((d|255-h))
