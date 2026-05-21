5f04490070525c8bc9f4a490492b7c0b71c53127
MrKay12 <quentin.lepoutre12@gmail.com>
2026-05-21 13:15:34 +0000

    refactor: simplify 0-resolver.sh by removing unnecessary checks

--- network_security/2x01_network_services/0-resolver.sh
+++ network_security/2x01_network_services/0-resolver.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-resolver=$(awk '/^nameserver/ {print $2; exit}' /etc/resolv.conf)
-
-if [ -z "$resolver" ] || [[ "$resolver" == "127.0.0.53" ]]; then
-    resolver=$(resolvectl status 2>/dev/null | awk '/DNS Servers:/ {print $3; exit}')
-fi
-
-echo "$resolver"
+awk '/^nameserver/ {print $2; exit}' /etc/resolv.conf
