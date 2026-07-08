e880dac3a053a81413dc4ebbb7662bff63566233
MrKay12 <quentin.lepoutre12@gmail.com>
2026-07-08 13:06:09 +0000

    update: refine 7-sqli.sh to use tshark display filter for SQL injection detection

--- network_security/2x03_traffic_forensics/7-sqli.sh
+++ network_security/2x03_traffic_forensics/7-sqli.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tshark -r "$1" -T fields -e http.request.uri | grep -Ei "union|select|%55%4e%49%4f%4e|%53%45%4c%45%43%54" \ndone
+tshark -r "$1" -Y 'http.request.uri matches "(?i)(UNION|SELECT)"' -T fields -e http.request.uri \done
