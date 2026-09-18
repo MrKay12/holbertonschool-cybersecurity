5a333cfcea73d53a60ae0248be1dd17c2f02d0f2
MrKay12 <quentin.lepoutre12@gmail.com>
2026-09-18 09:35:41 +0000

    refactor: simplify variable usage and improve HTML output formatting in report script

--- security_concepts/3x04_the_watchtower/9-html_report.sh
+++ security_concepts/3x04_the_watchtower/9-html_report.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-INPUT="$1"
-OUTPUT="$2"
-
-TOP_ATTACKERS=$(grep "Failed password" "$INPUT" \
+TOP_ATTACKERS=$(grep "Failed password" "$1" \
 | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' \
 | sort \
 | uniq -c \
@@ -14,25 +11,19 @@ TOP_ATTACKERS=$(grep "Failed password" "$INPUT" \
     echo '<!DOCTYPE html>'
     echo '<html>'
     echo '<head>'
-    echo '    <meta charset="UTF-8">'
-    echo '    <title>Security Report</title>'
+    echo '<meta charset="UTF-8">'
+    echo '<title>Security Report</title>'
     echo '</head>'
     echo '<body>'
-    echo '    <h1>Security Report</h1>'
-    echo '    <table border="1">'
-    echo '        <tr>'
-    echo '            <th>Attempts</th>'
-    echo '            <th>IP Address</th>'
-    echo '        </tr>'
+    echo '<h1>Security Report</h1>'
+    echo '<table border="1">'
+    echo '<tr><th>Attempts</th><th>IP Address</th></tr>'
 
     echo "$TOP_ATTACKERS" | awk '{
-        print "        <tr>"
-        print "            <td>" $1 "</td>"
-        print "            <td>" $2 "</td>"
-        print "        </tr>"
+        print "<tr><td>" $1 "</td><td>" $2 "</td></tr>"
     }'
 
-    echo '    </table>'
+    echo '</table>'
     echo '</body>'
     echo '</html>'
-} > "$OUTPUT"
+} > $2
