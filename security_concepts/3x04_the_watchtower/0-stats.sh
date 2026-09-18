80db04844dae594661e3605061f91cbd43648275
MrKay12 <quentin.lepoutre12@gmail.com>
2026-09-18 09:17:22 +0000

    add: create stats script for log file line counts and README file

---
+++ security_concepts/3x04_the_watchtower/0-stats.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "File: /var/log/auth.log - Lines: $(wc -l < /var/log/auth.log)"
+echo "File: /var/log/syslog - Lines: $(wc -l < /var/log/syslog)"
+echo "File: /var/log/kern.log - Lines: $(wc -l < /var/log/kern.log)"
